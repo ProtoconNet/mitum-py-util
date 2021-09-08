2073093cc3df86618f7426c849773d986ce4e8d4
wyuinche <wyuinche.mido@gmail.com>
2021-09-08 11:03:58 +0900

    Fix UTC parser; parseISOtoUTC

--- src/mitumc/common.py
+++ src/mitumc/common.py
@@ -10,22 +10,23 @@ class Int(object):
     Attributes:
         value (int): Integer by big endian byteorder
     """
+
     def __init__(self, value):
         self.value = value
-    
+
     def tight_bytes(self):
         # Converts int to N length bytes by big endian
         n = abs(self.value)
-        
+
         result = bytearray()
         while(n):
             result.append(n & 0xff)
             n = n >> 8
-        
+
         return bytes(result[::-1])
 
     def to_bytes(self):
-        # Converts int to 8 length bytes by big endian 
+        # Converts int to 8 length bytes by big endian
         n = int(self.value)
         count = 0
 
@@ -35,7 +36,7 @@ class Int(object):
             n = n >> 8
             count += 1
 
-        result =  result[::-1] + bytearray([0] * (8-count))
+        result = result[::-1] + bytearray([0] * (8-count))
         return bytes(result)
 
     def little4_to_bytes(self):
@@ -59,10 +60,11 @@ class Hint(object):
         h_type (str): type-hint
         h_ver  (str): mitum-currency version
     """
+
     def __init__(self, type, ver):
         self.h_type = type
         self.h_ver = ver
-    
+
     @property
     def type(self):
         return self.h_type
@@ -78,6 +80,7 @@ class Hash(object):
     Attributes:
         hs (binary): Hash digest in binary format
     """
+
     def __init__(self, hs):
         self.hs = hs
 
@@ -95,6 +98,7 @@ class Hash(object):
 def iso8601TimeStamp():
     return str(datetime.datetime.now(tz=pytz.utc).isoformat())
 
+
 def getNewToken(iso):
     idx = iso.find('+')
     if idx == -1:
@@ -102,10 +106,44 @@ def getNewToken(iso):
         exit(-1)
     return iso[:idx] + 'Z'
 
+
 def parseISOtoUTC(iso):
     t = iso.find('T')
-    date, at, z = iso[:t], iso[t+1:t+13], "+0000"
-    return date + " " + at + " " + z + " " + "UTC"
+    z = iso.find('Z')
+    rtime = ""
+
+    if z < 0:
+        z = iso.find('+')
+
+    assert z > -1, "[ERROR] Invalid ISO type for parseISOtoUTC"
+
+    _time = iso[t + 1: z]
+    if len(_time) > 12:
+        _time = _time[0: 12]
+
+    dot = _time.find('.')
+    if dot < 0:
+        rtime = _time
+    else:
+        decimal = _time[9: len(_time)]
+        idx = decimal.rfind('0')
+        if idx < 0 or idx != len(decimal) - 1:
+            rtime = _time
+        else:
+            startIdx = len(decimal) - 1
+            for i in range(len(decimal) - 1, -1, -1):
+                if decimal[i] == '0':
+                    startIdx = i
+                else:
+                    break
+            if startIdx == 0:
+                rtime = _time[0: dot]
+            else:
+                rtime = _time[0: dot] + '.' + decimal[0: startIdx]
+    
+    date, z = iso[:t], "+0000 UTC"
+
+    return date + " " + rtime + " " + z
 
 
 def bconcat(*blist):
@@ -113,24 +151,26 @@ def bconcat(*blist):
 
     Args:
         *blist: Arguments to concatenate
-    
+
     Returns:
         bytes: Concatenated bytes type instance
     """
     concated = bytearray()
-    
+
     for i in blist:
-        assert isinstance(i, bytes) or isinstance(i, bytearray), 'Arguments must be provided in bytes or bytearray format'
+        assert isinstance(i, bytes) or isinstance(
+            i, bytearray), 'Arguments must be provided in bytes or bytearray format'
         concated += bytearray(i)
-    
+
     return bytes(concated)
+
 
 def parseAddress(addr):
     """ Seperates address(or key) into type-hint and hintless address(key).
 
     Args:
         addr (str): Hinted address(or key)
-    
+
     Returns:
         type (str): Hint of address(or key)
         addr (str): Address(or key) without hint
@@ -159,7 +199,7 @@ def parseAddress(addr):
             if addr[i-1] != '-':
                 idx2 = -1
             break
-        
+
     assert idx2 != -1, 'Invalid hint'
 
     type = addr[idx+1:idx2-1]
