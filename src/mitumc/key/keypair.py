f53cf539161047a5a4940a45df59e6c624aa4732
wyuinche <wyuinche.mido@gmail.com>
2021-12-28 15:30:02 +0900

    Implement getKeypairFromSeed(); Keypair

--- src/mitumc/key/keypair.py
+++ src/mitumc/key/keypair.py
@@ -13,9 +13,8 @@ import ecdsa
 from ecdsa.curves import SECP256k1
 from ecdsa.util import sigencode_der_canonize
 
-from mitumc.hash import sha256
+from mitumc.hash import sha256, sha3
 from mitumc.hint import KEY_PRIVATE, KEY_PUBLIC
-from mitumc.key import Keypair
 from mitumc.key.base import BaseKey
 from mitumc.common import parseType
 
@@ -27,7 +26,6 @@ class Keypair:
             self.priv = BaseKey(KEY_PRIVATE, raw)
             self.seed = None
         else:
-            assert len(self.seed) >= 36, 'Seed is too short to create Keypair; Keypair'
             self.priv = None
             self.seed = priv
         self.generatePrivateKey()
@@ -35,12 +33,15 @@ class Keypair:
 
     def generatePrivateKey(self):
         if self.priv != None:
-            return
+            assert isinstance(
+                self.priv, BaseKey), 'Wrong private key or seed; Keypair'
         elif self.seed != None:
-            pass
-
-        assert isinstance(
-            self.priv, BaseKey), 'Wrong private key or seed; Keypair'
+            assert len(
+                self.seed) >= 36, 'Seed is too short to create Keypair; Keypair'
+            sh = sha3(self.seed.encode())
+            shb = base58.b58encode(sh.digest)[:-4]
+            k = str(hex((int.from_bytes(shb, "big") % (SECP256k1.order - 1)) + 1))[2:]
+            self.priv = BaseKey(KEY_PRIVATE, encodeKey(bytes.fromhex(k)))
 
     def generatePublicKey(self):
         wif = base58.b58encode_check(
@@ -70,16 +71,22 @@ class Keypair:
         return sk.sign(hs, hashfunc=hashlib.sha256, sigencode=sigencode_der_canonize)
 
 
+def encodeKey(key):
+    pk = b'\x80' + key + b'\x01'
+    return base58.b58encode(pk + checksum(pk)).decode()
+
+
 def getNewKeypair():
-    pk = b'\x80' + Wallet().key.digest + b'\x01'
-    wif = base58.b58encode(pk + checksum(pk)).decode()
-    return getKeypairFromPrivateKey(wif + KEY_PRIVATE)
+    return getKeypairFromPrivateKey(encodeKey(Wallet().key.digest) + KEY_PRIVATE)
+
 
 def getKeypairFromPrivateKey(priv):
     _, type = parseType(priv)
     assert type == KEY_PRIVATE, 'Not private key; getKeypairFromPrivateKey'
     return Keypair(priv, False)
 
+
 def getKeypairFromSeed(seed):
-    assert len(seed) >= 36, 'Seed is too short to create Keypair; getKeypairFromseed'
+    assert len(
+        seed) >= 36, 'Seed is too short to create Keypair; getKeypairFromseed'
     return Keypair(seed, True)
